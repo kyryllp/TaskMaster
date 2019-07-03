40782b77fe6045bcbdfaaae39267f92fb42cee78
cormillz <senokirill@gmail.com>
2019-07-03 23:21:13 +0300

    Changed host for Flask to Work with Docker. Tweaked css.

--- src/app.py
+++ src/app.py
@@ -102,4 +102,4 @@ def register():
 if __name__ == '__main__':
     mongo_setup.global_init()
     mongoengine.connect(db='simple_todo')
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
